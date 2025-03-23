d9895fed81233e24b35d3503f6c2a185e5e6e688
EL MAHDI BADILOU <mehdibadilou7@gmail.com>
2025-03-23 20:20:07 +0000

    fix(ui): -making the chatArea scrollable and the message boxes resizable

--- src/components/chatArea/chatArea.jsx
+++ src/components/chatArea/chatArea.jsx
@@ -1,12 +1,12 @@
 
 export default function ChatArea({messages}) {
     return (
-       <div className=" text-stone-100 h-460 bg-[#212121]">
+       <div className=" text-stone-100 h-460 bg-[#212121] overflow-x-auto">
               {messages.map((message, index) => (
-                  <div key={index} className="mb-3">
-                      <div className="w-200 h-12 bg-[#303030] text-stone-100">
-                          {message.content}
-                      </div>
+                  <div key={index} className="mb-3 w-200  bg-[#303030] text-stone-100">
+                         <p>
+                            {message.content}
+                         </p>
                   </div>
               ))}
        </div>
